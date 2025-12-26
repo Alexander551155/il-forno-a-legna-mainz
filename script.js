3fe27ba49386df3e7d7a482a397796b2f976e50b
Oleksandr Kononenko <135839545+Alexander551155@users.noreply.github.com>
2025-12-26 22:50:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ const yearSpan = document.getElementById("year");
 if (yearSpan) {
   yearSpan.textContent = new Date().getFullYear();
 }
+
+// Hero Slider (Fade)
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+const slideInterval = 5000; // 5 Sekunden pro Bild
+
+function nextSlide() {
+  slides[currentSlide].classList.remove("active");
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add("active");
+}
+
+setInterval(nextSlide, slideInterval);
